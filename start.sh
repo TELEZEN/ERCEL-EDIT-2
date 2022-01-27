82b414e0b35544b57a751bef1e3fbe8cb93bb8db
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-01-27 12:14:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TELEZEN/Ercel-monore.git /Ercel-monore
+  git clone https://github.com/TELEZEN/ERCEL-EDIT-2.git /ERCEL-EDIT-2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ercel-monore
+  git clone $UPSTREAM_REPO /ERCEL-EDIT-2
 fi
-cd /Ercel-monore
+cd /ERCEL-EDIT-2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
